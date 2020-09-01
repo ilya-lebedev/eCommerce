65448340b37bfbc4e782e1bddfddbf0bf2fb5922
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-09-01 08:50:38 +0300

    refactor: Replace stream with constructor

--- src/main/java/com/example/ecommerce/model/persistence/UserOrder.java
+++ src/main/java/com/example/ecommerce/model/persistence/UserOrder.java
@@ -1,8 +1,8 @@
 package com.example.ecommerce.model.persistence;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -75,7 +75,7 @@ public class UserOrder {
 
 	public static UserOrder createFromCart(Cart cart) {
 		UserOrder order = new UserOrder();
-		order.setItems(cart.getItems().stream().collect(Collectors.toList()));
+		order.setItems(new ArrayList<>(cart.getItems()));
 		order.setTotal(cart.getTotal());
 		order.setUser(cart.getUser());
 		return order;
