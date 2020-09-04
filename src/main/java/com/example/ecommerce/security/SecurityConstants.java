f7f4b05410fca68ba4736e121c39496f6bc4fc53
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-09-04 18:18:09 +0300

    feat: Implement security

---
+++ src/main/java/com/example/ecommerce/security/SecurityConstants.java
@@ -0,0 +1,11 @@
+package com.example.ecommerce.security;
+
+public class SecurityConstants {
+
+    public static final String SECRET = "secret_key";
+    public static final long EXPIRATION_TIME = 864_00_00; // 10 days
+    public static final String TOKEN_PREFIX = "Bearer";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/api/user/create";
+
+}
